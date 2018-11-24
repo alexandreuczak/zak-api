300c8565ce6c6af8826dc5c0353673ab12870f51
Alexandre Uczak <alexandre.uczak@gmail.com>
2018-11-24 21:16:44 -0200

    create delete verb

--- src/main/java/br/com/zak/api/rest/GreetingController.java
+++ src/main/java/br/com/zak/api/rest/GreetingController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zak.api.domain.Greeting;
@@ -34,4 +37,9 @@ public class GreetingController {
 		return new ResponseEntity<List<Greeting>>(greetings, HttpStatus.OK);
 	}
 	
+	@DeleteMapping
+	public ResponseEntity<Void> remover(@RequestParam("id") Long id){
+		repository.deleteById(id);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 }
