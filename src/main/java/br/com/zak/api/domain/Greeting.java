1c1160e0b68ee858c4eac591814f9829b3171e70
Alexandre Uczak <alexandre.uczak@gmail.com>
2018-11-24 16:26:54 -0200

    mapping entity

--- src/main/java/br/com/zak/api/domain/Greeting.java
+++ src/main/java/br/com/zak/api/domain/Greeting.java
@@ -2,12 +2,22 @@ package br.com.zak.api.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name  = "gretting")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,6 +27,13 @@ public class Greeting implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gretting_seq")
+	@SequenceGenerator(name = "gretting_seq", sequenceName = "gretting_seq")
+	private Long id;
+	
+	@Column(name = "message")
 	private String message;
 	
 }
