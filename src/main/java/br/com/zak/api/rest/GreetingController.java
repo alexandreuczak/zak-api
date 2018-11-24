d91625e3b55ebc9dc5c67f2d1726244181476775
Alexandre Uczak <alexandre.uczak@gmail.com>
2018-11-24 18:15:02 -0200

    refact app context

--- src/main/java/br/com/zak/api/rest/GreetingController.java
+++ src/main/java/br/com/zak/api/rest/GreetingController.java
@@ -6,20 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zak.api.domain.Greeting;
 import br.com.zak.api.repository.GrettingRepository;
 
-@RestController
-public class HelloWorldController {
+@RestController("/greeting")
+public class GreetingController {
 
 	@Autowired
 	private GrettingRepository repository;
 	
-	@GetMapping("/hello/{message}")
-	public ResponseEntity<Greeting> hello(@PathVariable("message") String message){
+	@PostMapping
+	public ResponseEntity<Greeting> hello(@RequestParam("message") String message){
 		Greeting greeting = Greeting.builder().message(message).build();
 		
 		repository.save(greeting);
@@ -27,7 +28,7 @@ public class HelloWorldController {
 		return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);		
 	}
 
-	@GetMapping("/grettings")
+	@GetMapping
 	public ResponseEntity<List<Greeting>> listar(){
 		List<Greeting> greetings = repository.findAll();
 		return new ResponseEntity<List<Greeting>>(greetings, HttpStatus.OK);
