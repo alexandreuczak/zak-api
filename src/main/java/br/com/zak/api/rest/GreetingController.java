9b0279f622ae4e4c966ed3ea25fd5f7adbae5530
Alexandre Uczak <alexandre.uczak@gmail.com>
2018-11-25 16:49:36 -0200

    config swagger-ui

--- src/main/java/br/com/zak/api/rest/GreetingController.java
+++ src/main/java/br/com/zak/api/rest/GreetingController.java
@@ -8,19 +8,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zak.api.domain.Greeting;
 import br.com.zak.api.repository.GrettingRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
-@RestController("/greeting")
+@Api("Zak API Rest")
+@RestController
+@RequestMapping("/greeting")
 public class GreetingController {
 
 	@Autowired
 	private GrettingRepository repository;
 	
-	@PostMapping
+	@ApiOperation("Create a new greeting")
+	@PostMapping(produces = "application/json")
 	public ResponseEntity<Greeting> hello(@RequestParam("message") String message){
 		Greeting greeting = Greeting.builder().message(message).build();
 		
@@ -29,12 +35,14 @@ public class GreetingController {
 		return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);		
 	}
 
-	@GetMapping
+	@ApiOperation("List all greetings")
+	@GetMapping(produces = "application/json")
 	public ResponseEntity<List<Greeting>> listar(){
 		List<Greeting> greetings = repository.findAll();
 		return new ResponseEntity<List<Greeting>>(greetings, HttpStatus.OK);
 	}
 	
+	@ApiOperation("Remove a greeting")
 	@DeleteMapping
 	public ResponseEntity<Void> remover(@RequestParam("id") Long id){
 		repository.deleteById(id);
