ada536c990f572ea29c1f8948f9f803a37ac5da6
Alexandre Uczak <alexandre.uczak@gmail.com>
2018-11-25 18:55:49 -0200

    fix rest exchange

--- src/main/java/br/com/zak/api/service/PokemonService.java
+++ src/main/java/br/com/zak/api/service/PokemonService.java
@@ -1,5 +1,11 @@
 package br.com.zak.api.service;
 
+import java.util.Arrays;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +18,16 @@ public class PokemonService {
 	
 	public PokemonResponse getAll() {
 		RestTemplate restTemplate = new RestTemplate();
-		return restTemplate.getForObject(POKEMON_URI, PokemonResponse.class);
+		
+		 HttpHeaders headers = new HttpHeaders();
+	        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+	        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		
+//		return restTemplate.getForObject(POKEMON_URI, PokemonResponse.class);
+		return restTemplate
+				.exchange(POKEMON_URI, HttpMethod.GET , entity,PokemonResponse.class)
+				.getBody();
 	}
 	
 }
