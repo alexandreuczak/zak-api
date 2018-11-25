df5a4f449947b7e7472caf1922596e4ff6efe587
Alexandre Uczak <alexandre.uczak@gmail.com>
2018-11-25 17:21:12 -0200

    condition load data.sql

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,3 @@
+insert into gretting(id, message)values(gretting_seq.nextval, 'olá');
+insert into gretting(id, message)values(gretting_seq.nextval, 'oi');
+insert into gretting(id, message)values(gretting_seq.nextval, 'tudo bem');
