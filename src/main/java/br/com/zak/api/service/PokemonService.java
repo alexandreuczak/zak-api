8e168ca0e2378a25b24d1cc9e9ed56bb63ffc0c2
Alexandre Uczak <alexandre.uczak@gmail.com>
2018-11-25 19:09:47 -0200

    refactoring constants

--- src/main/java/br/com/zak/api/service/PokemonService.java
+++ src/main/java/br/com/zak/api/service/PokemonService.java
@@ -15,17 +15,19 @@ import br.com.zak.api.response.PokemonResponse;
 public class PokemonService {
 
 	private static final String POKEMON_URI = "https://pokeapi.co/api/v2/pokemon/";
+	private static final String USER_AGENT_HEADER = "user-agent";
+	private static final String USER_AGENT_HEADER_BROWSER_INFO = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36";
 	
 	public PokemonResponse getAll() {
 		RestTemplate restTemplate = new RestTemplate();
 		
 		 HttpHeaders headers = new HttpHeaders();
 	        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+	        headers.add(USER_AGENT_HEADER, USER_AGENT_HEADER_BROWSER_INFO);
 	        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 		
 		return restTemplate
-				.exchange(POKEMON_URI, HttpMethod.GET , entity,PokemonResponse.class)
+				.exchange(POKEMON_URI, HttpMethod.GET, entity, PokemonResponse.class)
 				.getBody();
 	}
 	
