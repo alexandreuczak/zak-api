3e4ed85fcce7fc037dc0e6e89783692272f45037
Alexandre Uczak <alexandre.uczak@gmail.com>
2018-12-02 21:06:28 -0200

    discard ure path authorization

--- src/main/java/br/com/zak/api/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/zak/api/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,9 +20,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.anyRequest().authenticated()
-			.and().httpBasic().and().csrf().disable();
+		http.authorizeRequests().anyRequest().authenticated()	
+			.and().httpBasic()
+			.and().csrf().disable();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/user");
 	}
 	
 	@Override
